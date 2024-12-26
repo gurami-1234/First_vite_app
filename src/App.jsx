ca5f448aed234eef7aca7e56169649a7fb4bd433
gurami-1234 <gukadavitadze22@gmail.com>
2024-12-26 20:08:08 +0400

    First vite commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./Components/Navbar/Navbar";
+function App() {
+  return (
+    <>
+      <Navbar />
+      <h1>First App</h1>
+      <h2>Secondary Title</h2>
+    </>
+  );
+}
+
+export default App;
